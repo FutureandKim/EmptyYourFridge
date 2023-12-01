f085e3fd663804ac601f9c81a2bd1dbc6728c353
MiraeKim <hufs202000512@gmail.com>
2023-12-02 00:47:50 +0900

    list -> grid

--- app/src/main/java/com/example/emptyyourfridge/IngredientsActivity.kt
+++ app/src/main/java/com/example/emptyyourfridge/IngredientsActivity.kt
@@ -2,19 +2,17 @@ package com.example.emptyyourfridge
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.ListView
+import android.widget.GridView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 class IngredientsActivity : AppCompatActivity() {
 
-    private val foodItems = arrayOf("달걀", "콩나물", "두부", "햄", "양파", "감자", "고구마", "토마토", "참치", "배추", "오이")
+    //ingredients list
+    private val foodItems = arrayOf("당근", "감자", "양파", "마늘", "브로콜리", "고구마", "시금치", " ", "소고기", "돼지고기", "우유", "콩")
     private val selectedFoodList = ArrayList<String>()
     private lateinit var selectedFoodsTextView: TextView
 
@@ -22,29 +20,28 @@ class IngredientsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingredients)
 
-        val listView: ListView = findViewById(R.id.ingredientsListView)
+        val gridView: GridView = findViewById(R.id.ingredientsGridView)
         selectedFoodsTextView = findViewById(R.id.selectedFoodsTextView)
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, foodItems)
-        listView.adapter = adapter
+        gridView.adapter = adapter
 
-        listView.setOnItemClickListener { _, _, position, _ ->
+        gridView.setOnItemClickListener { _, _, position, _ ->
             val selectedFood = foodItems[position]
 
-            if (listView.isItemChecked(position)) {
-                // 선택된 음식 추가
+            if (gridView.isItemChecked(position)) {
+                // 선택한 음식 추가
                 selectedFoodList.add(selectedFood)
             } else {
-                // 선택 해제된 음식 제거
+                // 선택 해제한 음식 제거
                 selectedFoodList.remove(selectedFood)
             }
 
             updateSelectedFoodsText()
-            // 추가 동작을 수행
+            // 추가 동작
         }
 
         val nextButton: Button = findViewById(R.id.nextButton)
-
         nextButton.setOnClickListener{
             moveToCategoryActivity()
         }
@@ -63,6 +60,7 @@ class IngredientsActivity : AppCompatActivity() {
         // 다음 화면을 시작합니다.
         startActivity(intent)
     }
+
 }
 
 
