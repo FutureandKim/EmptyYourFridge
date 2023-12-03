c38470418b25d519323ad38507948075431471c6
MiraeKim <hufs202000512@gmail.com>
2023-12-03 21:54:45 +0900

    feat: basic setting

--- app/src/main/java/com/example/emptyyourfridge/CategoryActivity.kt
+++ app/src/main/java/com/example/emptyyourfridge/CategoryActivity.kt
@@ -2,10 +2,24 @@ package com.example.emptyyourfridge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+        // get data by Intent
+        val selectedFoodList = intent.getStringArrayListExtra("selectedFoodList")
+
+        // UI에서 데이터를 표시할 TextView
+        val selectedFoodTextView = findViewById<TextView>(R.id.selectedFoodTextView)
+
+        // 받아온 데이터를 TextView에 설정
+        if (selectedFoodList != null) {
+            val foodListText = "Selected Food List: ${selectedFoodList.joinToString(", ")}"
+            selectedFoodTextView.text = foodListText
+        }
     }
 }
